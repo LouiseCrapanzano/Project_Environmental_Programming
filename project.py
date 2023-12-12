0c51ae944437ce1f329fe738397f9f81137cfaca
LouiseCrapanzano <louise.crapanzano.minichello@vub.be>
2023-12-12 11:34:47 +0100

    eerste commit python

---
+++ project.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 12 10:53:46 2023
+
+@author: louis
+"""
+
+#task 2 project
+#eerst pandas importeren
+import pandas as pd
+
+#dataframe maken
+print(pd.__version__)
