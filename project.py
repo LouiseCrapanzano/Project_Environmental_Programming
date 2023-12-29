be354d49b5d7678e8d21e5539e4775849d8357a2
livacke <liv.acke@vub.be>
2023-12-29 15:51:58 +0100

    Update project.py

--- project.py
+++ project.py
@@ -51,7 +51,7 @@ satellite["date"]=satellite["filename"].str[11:19]
 print(satellite)
 
 # Path_Liv = /Users/livacke/Library/CloudStorage/OneDrive-VrijeUniversiteitBrussel/3e Bachelor/Environmental Programming/Clipped_data
-# Path_Louis = r'C:\Users\louis\Downloads\EP_Project\Data' 
+# Path_Louis = r'C:/Users/louis/Downloads/EP_Project/Data' 
 # Path_Alex = /Users/alexsamyn/Library/CloudStorage/OneDrive-Gedeeldebibliotheken-VrijeUniversiteitBrussel/Liv Acke - Environmental Programming/Clipped_data
 
 ## Task 4 and Task 5
