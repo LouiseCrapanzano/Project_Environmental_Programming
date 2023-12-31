c73fdd44d7d3744404b0f0015a1e3ed27761ba7d
LouiseCrapanzano <louise.crapanzano.minichello@vub.be>
2023-12-31 12:35:22 +0100

    Update project.py

--- project.py
+++ project.py
@@ -5,9 +5,9 @@ Created on Tue Dec 12 10:53:46 2023
 @author: louis, livacke
 """
 
-# Task 2 
+## Task 2 
 
-# first import libraries
+# First import libraries
 import geopandas as gpd
 import pandas as pd
 import zipfile
@@ -20,32 +20,32 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import rasterstats
 
-# ask user for directory where data is stored
+# Ask user for directory where data is stored
 currentdir = input('What is the path to your unzipped file (Clipped data) on your computer?\n')
 
 items_dir = []
-items = os.listdir(currentdir) # code looks inside current directory/folder and searches all other directories
+items = os.listdir(currentdir) # Code looks inside current directory/folder and searches all other directories
 for item in items:
     path = os.path.join(currentdir, item)
-    if item[-4:]=='.zip':      # code to unzip zipfiles if present
+    if item[-4:]=='.zip':      # Code to unzip zipfiles if present
         with zipfile.ZipFile(path,"r") as zip_ref:
             zip_ref.extractall(currentdir)
         os.remove(path)
     if (os.path.isdir(os.path.join(currentdir, item))):
         
-        if(item[:3]=='R08'): #only look for folders with filename that start with 'R08'
+        if(item[:3]=='R08'): # Only look for folders with filename that start with 'R08'
             items_R08=sorted(os.listdir(path))
         if(item[:2]=='S2'):
             items_dir.append(item)
             items_dir = sorted(items_dir)
     
-# create a dataframe and store lists inside the dataframe 
+# Create a DataFrame and store lists inside the DataFrame 
 satellite = pd.DataFrame({"filename": items_dir, "Band08": items_R08})
 
-# extract date from filename (3rd column dataframe)
+# Extract date from filename (3rd column DataFrame)
 satellite["date"]=satellite["filename"].str[11:19]
 
-# sort dataframe chronologically based on date
+# Sort DataFrame chronologically based on date
 satellite = satellite.sort_values("date")
 
 print(satellite)
@@ -97,7 +97,7 @@ def CalcRaster(A_p, C_p, Param, currentdir, Band, date, Display=False):
     condition = ((RasterData_unfiltered != 65535) & (RasterData_unfiltered > 0) & (RasterData_unfiltered < 100))
     RasterData = np.ma.masked_array(RasterData_unfiltered, mask=~condition)
     
-    out_meta.update({'driver':'GTiff',         # adapt value of variables to desired
+    out_meta.update({'driver':'GTiff',         # Adapt value of variables to desired
                      'width':rho.shape[1],
                      'height':rho.shape[0],
                      'count':1,
@@ -108,9 +108,9 @@ def CalcRaster(A_p, C_p, Param, currentdir, Band, date, Display=False):
     
     return RasterData, out_meta
 
-# save data (task 5)
+# Save data (task 5)
 def SaveRaster(RasterData, out_meta, currentdir, Param, date):    
-    path_out = currentdir + f'/{Param}/{Param}_{date}.tif' # path of where you want to save raster data
+    path_out = currentdir + f'/{Param}/{Param}_{date}.tif' # Path of where you want to save raster data
     with rasterio.open(fp=path_out, # outputpath_name
                   mode='w',**out_meta) as dst:
                   dst.write(RasterData, 1)
@@ -129,7 +129,7 @@ def PlotRaster(RasterData, Param, date, Display):
      
             plt.show()      
 
-# make the folders where RasterData is saved, if not created already (Task 5)
+# Make the folders where RasterData is saved, if not created already (Task 5)
 def CreateFolder(currentdir, Param):
     path_create = os.path.join(currentdir,Param)
     if not os.path.exists(path_create):
@@ -145,7 +145,7 @@ for Band in All_Band08:
     
     # Update expected dimensions based on clipped_data
     
-    # calculate TUR:
+    # Calculate TUR:
     A_p = 1602.93
     C_p = 0.19130
     Param = 'TUR'
@@ -154,9 +154,7 @@ for Band in All_Band08:
     SaveRaster(TUR_data, out_meta, currentdir, Param, date[0])
     PlotRaster(TUR_data, Param, date[0], True)
     
-    # constant variables for both parameters (TUR and SPM)
-    
-    # calculate SPM:
+    # Calculate SPM:
     A_p = 1801.52
     C_p = 0.19130
     Param = 'SPM'
@@ -213,7 +211,7 @@ satellite = gpd.GeoDataFrame(pd.concat([concatenated_geodataframes, concatenated
 # Print the final GeoDataFrame
 print(satellite)
 
-# Task 8
+## Task 8
 # Getting the columns out of the DataFrame
 years = satellite['date']
 SPM_mean = satellite['SPMmean']
